2570ca268c159c349989dde41386e7644b89ee3b
Nguyễn Develop <nguyendevelop@hotmail.com>
2022-03-23 19:54:27 +0700

    update

---
+++ NarfuQuote/NarfuQuote/NarfuQuoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  NarfuQuoteApp.swift
+//  NarfuQuote
+//
+//  Created by Nguyen Dang Quy on 23/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct NarfuQuoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
